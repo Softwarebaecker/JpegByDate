8f7c54a8ab4df44cf6b3c5a96c512f2d7eda95f4
stonewalker92 <stonewalker92@gmail.com>
2016-10-24 18:18:06 +0200

    tested project status
    Features:
    - running Project
    - parsing of input parameters
    - error handling
    - searching all jpeg files in a directory

--- src/param.ads
+++ src/param.ads
@@ -2,7 +2,7 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 Package Param is
 
-   --type params is tagged private;
+   --in that type all input parameter information are stored
    type params is
      tagged record
        isWildcard : Boolean := false;
@@ -11,9 +11,11 @@ Package Param is
        fileName : Unbounded_String;
        directory : Unbounded_String := To_Unbounded_String(".");
        isWholePath : Boolean := false;
-      end record;
+   end record;
+   --reads all input parameters, extracts the information and stores it
+   --inside of the type params.
    procedure initialize;
+   --makes it possible to access the type params
    function getParams return params;
-
 
 end Param;
