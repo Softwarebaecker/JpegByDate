f57564b3a31b33022e40773a4057998a0729169b
Softwarebaecker <nil.kuchenbaecker@googlemail.com>
2016-11-13 15:20:26 +0100

    read Date + Time
    ImageFile open the file and read the date and the time.
    create return a pointer to ImageFile.ExifData

--- src/ImageFile.adb
+++ src/ImageFile.adb
@@ -5,17 +5,25 @@
 
 Package body ImageFile is
 
-   function create(fileName : String) return access ExifData is
-      Buffer : Character;
-      PositionTemplate : Short_Integer;
+   function create(fileName : Unbounded_String) return access ExifData is
       ExifDatas : access ExifData := new ExifData;
    begin
+      ExifDatas.filename := fileName;
       --open FileStream
-      Ada.Streams.Stream_IO.Open(File, Ada.Streams.Stream_IO.In_File, filename);
+      Ada.Streams.Stream_IO.Open(File, Ada.Streams.Stream_IO.In_File,
+                                 Ada.Strings.Unbounded.To_String(filename));
       Input_Stream := Ada.Streams.Stream_IO.Stream(File);
 
+      if checkFile = FALSE then
+         Close(File);
+         return null;
+      end if;
 
+      --read Format
+      readFormat;
 
+      --read Date and Time
+      readDateTime(ExifDatas);
 
       Close(File);
       return ExifDatas;
@@ -23,17 +31,16 @@ Package body ImageFile is
    end create;
 
    function checkFile return Boolean is
-
-
    begin
       --Jpeg Beginn FF D8
       if Character'Input(Input_Stream) /= Character'Val(16#FF#) and
         Character'Input(Input_Stream) /= Character'Val(16#D8#) then
          --No Jepeg
          return FALSE;
+
       end if;
 
-      Set_Index(File, Ada.Streams.Stream_IO.Size(File)-2);
+      Set_Index(File, Ada.Streams.Stream_IO.Size(File)-1);
       --Jpeg End FF D9
       if Character'Input(Input_Stream) /= Character'Val(16#FF#) and
         Character'Input(Input_Stream) /= Character'Val(16#D9#) then
@@ -41,45 +48,130 @@ Package body ImageFile is
          return FALSE;
       end if;
 
-
-      Set_Index(File, 7);
-      --read Exif Header
-      if Character'Input(Input_Stream) /= Character'Val(16#45#) or
-        Character'Input(Input_Stream) /= Character'Val(16#78#) or
-        Character'Input(Input_Stream) /= Character'Val(16#69#) or
-        Character'Input(Input_Stream) /= Character'Val(16#66#) or
-        Character'Input(Input_Stream) /= Character'Val(16#00#) or
-        Character'Input(Input_Stream) /= Character'Val(16#00#) then
-         --throw exception
-         Put_Line("No EXIF Header");
-         return False;
-      end if;
-      -- Intel: 49 49 2a 00 08 00 00 00 = LLHH
-      -- TI:    4d 4d 00 2a 00 00 00 08 = HHLL
-
-                end checkFile;
-
-
-   function readDateTime return Boolean is
-   begin
-
+      --find Exif Header
+      Set_Index(File, 1);
+      while Index(File) <= Size(File) - 6 loop
+      if Character'Input(Input_Stream) = Character'Val(16#45#) and then
+           Character'Input(Input_Stream) = Character'Val(16#78#) and then
+           Character'Input(Input_Stream) = Character'Val(16#69#) and then
+           Character'Input(Input_Stream) = Character'Val(16#66#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) then
+      -- save the endposition of the TIFF Header
+      TIFFHeaderEndPos := Index(File) - 1;
+            return TRUE;
+         end if;
+      end loop;
       return FALSE;
+   end checkFile;
+
+
+   procedure readFormat is
+   begin
+      -- Intel: 49 49 2a 00 08 00 00 00 = LLHH
+      -- Motorola:    4d 4d 00 2a 00 00 00 08 = HHLL
+      if Character'Input(Input_Stream) = Character'Val(16#49#) and
+        Character'Input(Input_Stream) = Character'Val(16#49#) and
+        Character'Input(Input_Stream) = Character'Val(16#2a#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#08#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) then
+         --set to Intel Format
+         littleEndian := FALSE;
+      else if Character'Input(Input_Stream) = Character'Val(16#4d#) and
+        Character'Input(Input_Stream) = Character'Val(16#4d#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#2a#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#00#) and
+        Character'Input(Input_Stream) = Character'Val(16#08#) then
+         --set Motorola Format
+            littleEndian := FALSE;
+         end if;
+      end if;
+   end readFormat;
+
+
+   procedure readDateTime(ExifDatas : access ExifData) is
+      DatePosition : Integer;
+   begin
+      Set_Index(File,TIFFHeaderEndPos);
+      while Index(File) <= Size(File) - 6 loop
+         if Character'Input(Input_Stream) = Character'Val(16#90#) and then
+           Character'Input(Input_Stream) = Character'Val(16#03#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) and then
+           Character'Input(Input_Stream) = Character'Val(16#02#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) and then
+           Character'Input(Input_Stream) = Character'Val(16#00#) and then
+           Character'Input(Input_Stream) = Character'Val(16#14#) then
+
+            --read Position
+            DatePosition := readInt(Index(File), 4);
+            DatePosition := readInt(Index(File), 4) + 1;
+
+            --read Date
+            ExifDatas.date := readString(Ada.Streams.Stream_IO.Positive_Count(DatePosition) + TIFFHeaderEndPos, 10);
+            ExifDatas.date := convertDate(ExifDatas.date);
+            --read Time
+            ExifDatas.Time := readString(Ada.Streams.Stream_IO.Positive_Count(DatePosition) + 11 + TIFFHeaderEndPos, 8);
+         end if;
+      end loop;
 
    end readDateTime;
 
 
-   function readInt(position : Integer) return Integer is
+   function readInt(Position : Ada.Streams.Stream_IO.Positive_Count ;
+                    ByteCount : Integer) return Integer is
+      firstCharacter : Character;
+      secondCharacter : Character;
    begin
+      --set Index to Position
+      Set_Index(File,Position);
 
-      return 0;
+      --read two byts
+      firstCharacter := Character'Input(Input_Stream);
+      secondCharacter := Character'Input(Input_Stream);
 
+      --calculate Integer
+      if littleEndian = TRUE then --litteleendian LLHH
+         return Character'Pos(secondCharacter) * 16#100#
+         + Character'Pos(firstCharacter);
+      else --bigendian HHLL
+         return Character'Pos(firstCharacter) * 16#100#
+         + Character'Pos(secondCharacter);
+      end if;
    end readInt;
 
-   function readCharakter(position : Integer) return Character is
+   function readString(Position : Ada.Streams.Stream_IO.Positive_Count;
+                       Length : Integer) return String is
+      Counter : Integer := 1;
+      Return_String : Unbounded_String;
    begin
+      --set Index to Position
+      Set_Index(File,Position);
+      --read Charakter
+      while Counter <= Length loop
+         Ada.Strings.Unbounded.Append(Return_String, Character'Input(Input_Stream));
+         Counter := Counter + 1;
+      end loop;
 
-      return 'c';
+      --Ada.Text_IO.Put_Line(Ada.Strings.Unbounded.To_String(Return_String));
+      return Ada.Strings.Unbounded.To_String(Return_String);
 
-   end readCharakter;
+   end readString;
+
+   function convertDate(Date : String) return String is
+      returnString : String(1..10) := Date;
+   begin
+      returnString := Ada.Strings.Fixed.Overwrite(returnString, 5, "-");
+      returnString := Ada.Strings.Fixed.Overwrite(returnString, 8, "-");
+
+      return returnString;
+   end convertDate;
+
 
 end ImageFile;
--- src/ImageFile.ads
+++ src/ImageFile.ads
@@ -1,32 +1,36 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
+with Ada.Strings.Bounded;
+with Ada.Strings.Fixed;
 
 Package ImageFile is
 
    type ExifData is
      tagged record
       filename : Unbounded_String;
-      date : Unbounded_String;
-      Time : Unbounded_String;
+      date : String(1..10);
+      Time : String(1..8);
 
    end record;
 
    --Konstruktor
-   function create(fileName : String) return access ExifData;
+   function create(fileName : Unbounded_String) return access ExifData;
 
   private
    littleEndian : Boolean;
    File : Ada.Streams.Stream_IO.File_Type;
    Input_Stream : Ada.Streams.Stream_IO.Stream_Access;
+   TIFFHeaderEndPos : Ada.Streams.Stream_IO.Positive_Count;
 
    function checkFile return Boolean;
+   procedure readFormat;
+   procedure readDateTime(ExifDatas : access ExifData);
 
-   function readDateTime return Boolean;
-
-   function readInt(position : Integer) return Integer;
-   function readCharakter(position : Integer) return Character;
-
-
+   function readInt(Position : Ada.Streams.Stream_IO.Positive_Count;
+                    ByteCount : Integer) return Integer;
+   function readString(Position : Ada.Streams.Stream_IO.Positive_Count;
+                       Length : Integer) return String;
+   function convertDate(Date : String) return String;
 
 end ImageFile;
