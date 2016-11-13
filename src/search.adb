12c23bbd1974ec6f0a9b333b646595e6589ccacf
adnors <sandro.kaeppner@gmail.com>
2016-11-13 18:24:33 +0100

    Use of the ImageFile
    now uses the ImageFile to filter the found files

--- src/search.adb
+++ src/search.adb
@@ -1,25 +1,43 @@
-with Ada.Directories; use Ada.Directories;
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 Package body Search is
-
-   procedure Search_Directory --See https://rosettacode.org/wiki/Walk_a_directory/Non-recursively#Ada
-     (parameters : Param.params)
+   --See https://rosettacode.org/wiki/Walk_a_directory/Non-recursively#Ada
+   function searchDirectory (parameters : Param.params) return FileVector.Vector
      --searches the directory Directory for .jpg files and prints files with the pattern Pattern
    is
-      currentSearch : Search_Type;
-      dirEnt : Directory_Entry_Type;
+      currentSearch : Ada.Directories.Search_Type;
+      dirEnt : Ada.Directories.Directory_Entry_Type;
+      exifData : ImageFile.ExifDataAccess;
+      fileVectorObj : FileVector.Vector;
    begin
-      Start_Search(Search    => currentSearch,
-                   Directory => To_String(parameters.directory),
-                   Pattern   => filePattern);
-
-      while More_Entries(currentSearch) loop
-         Get_Next_Entry(currentSearch, dirEnt);
-         Put_Line(Simple_Name(dirEnt));
+      --get all files in the directory
+      Ada.Directories.Start_Search(Search    => currentSearch,
+                                   Directory => Ada.Strings.Unbounded.To_String(parameters.directory),
+                                   Pattern   => filePattern);
+      --filter the correct files
+      while Ada.Directories.More_Entries(currentSearch) loop
+         Ada.Directories.Get_Next_Entry(currentSearch, dirEnt);
+         --only call create if dirEnt is a readable file
+         if Ada.Directories.Kind(Directory_Entry => dirEnt) = Ada.Directories.Ordinary_File then
+            exifData := ImageFile.create(Ada.Strings.Unbounded.To_Unbounded_String(Ada.Directories.Full_Name(dirEnt)));
+            --add file to the vector if it fits the parameters
+            if exifData /= null and then exifData.date = Ada.Strings.Unbounded.To_String(parameters.date) then
+               FileVector.Append(Container => fileVectorObj,
+                                 New_Item  => exifData);
+            end if;
+         end if;
       end loop;
 
-      End_Search(currentSearch);
-   end Search_Directory;
+      Ada.Directories.End_Search(currentSearch);
+      return fileVectorObj;
+   end searchDirectory;
+
+   --function for the use with vectors
+   function "="(imageFile1, imageFile2 : ImageFile.ExifDataAccess) return Boolean is
+   begin
+      if imageFile1.filename = imageFile2.filename then
+         return True;
+      else
+         return False;
+      end if;
+   end "=";
+
 end Search;
--- src/search.ads
+++ src/search.ads
@@ -1,8 +1,16 @@
+with Ada.Containers.Vectors;
+with Ada.Directories; use Ada.Directories;
+with Ada.Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+with ImageFile;
 with Param;
 
 Package Search is
-   procedure Search_Directory
-     (parameters : Param.params);
+   function "="(imageFile1, imageFile2 : ImageFile.ExifDataAccess) return Boolean;
+   Package FileVector is new Ada.Containers.Vectors(Index_Type   => Natural,
+                                                    Element_Type => ImageFile.ExifDataAccess);
+   function searchDirectory(parameters : Param.params) return FileVector.Vector;
 private
-   filePattern : String := "*.jpg";
+   filePattern : String := "";
 end Search;
