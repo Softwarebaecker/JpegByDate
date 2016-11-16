5854c7520df9037c525af4f26d00e17dda588f77
unknown <mayer.stefan-tit14@ET111.it.ba-ravensburg.de>
2016-11-16 10:31:48 +0100

    Added comments to uicontroller body

--- src/uiController.adb
+++ src/uiController.adb
@@ -1,6 +1,10 @@
+--The uicontroller handles the communication between the search-option and the
+--output on an user-called interface. This handling is set throught parameters
+--inserted from the user.
 Package body UIController is
- vec : FileVector.Vector;
+   vec : FileVector.Vector;
 
+   --this procedure starts the search and gives to received vector to the cli
    procedure startFileSearch(parameter : Param.params)is
       vec : FileVector.Vector;
    begin
@@ -9,7 +13,8 @@ Package body UIController is
 
    end startFileSearch;
 
-
+   --starts the output on the cli. this procedure is needed if another kind of
+   --output should get implemented
    procedure startUI(vec : FileVector.Vector) is
 
    begin
