b2a019dbb0b45df255ab82ef46df70e02c17720b
stonewalker92 <stonewalker92@gmail.com>
2016-11-16 10:19:48 +0100

    start.adb

--- src/start.adb
+++ src/start.adb
@@ -10,12 +10,7 @@ with UIController; use UIController;
 procedure start is
    i : Integer := 0;
    parameter : aliased params;
-<<<<<<< HEAD
    input : Param.Input(1 .. Argument_Count);
-=======
-   vec : FileVector.Vector;
->>>>>>> refs/remotes/origin/develop
-
 begin
    --Copy passed arguments in array
    for i in 1 .. Argument_Count loop
@@ -25,34 +20,27 @@ begin
    --Evaluating the passed arguments
    initialize(Argument_Count, input);
    parameter := getParams;
-<<<<<<< HEAD
+
+   UIController.startFileSearch(parameter);
 
    --Output debug information
-   Put_Line("Debug Information:");
-   Put_Line("");
-   Put("Date is ");
-   Put_Line(To_String(parameter.date));
-   Put("Help is ");
-   Put_Line(Boolean' Image(parameter.isHelp));
-   Put("Recursiv is ");
-   Put_Line(Boolean' Image(parameter.isRecursive));
-   Put("File Name is ");
-   Put_Line(To_String(parameter.fileName));
-   Put("Directory is ");
-   Put_Line(To_String(parameter.directory));
-   Put("Whole path is ");
-   Put_Line(Boolean' Image(parameter.isWholePath));
+   --Put_Line("Debug Information:");
+   --Put_Line("");
+   --Put("Date is ");
+   --Put_Line(To_String(parameter.date));
+   --Put("Help is ");
+   --Put_Line(Boolean' Image(parameter.isHelp));
+   --Put("Recursiv is ");
+   --Put_Line(Boolean' Image(parameter.isRecursive));
+   --Put("File Name is ");
+   --Put_Line(To_String(parameter.fileName));
+   --Put("Directory is ");
+   --Put_Line(To_String(parameter.directory));
+   --Put("Whole path is ");
+   --Put_Line(Boolean' Image(parameter.isWholePath));
 
-   --Start search
-   Search_Directory(parameter);
-
-exception
-   --Not valid input
-=======
-   startFileSearch(parameter);
 
    exception
->>>>>>> refs/remotes/origin/develop
    when Constraint_Error =>
       Put_Line("");
       Put_Line("ERROR: Some parameters could not be loaded!");
