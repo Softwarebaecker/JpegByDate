1d84394ab0a74299369352a0ab6b1429ff8e8904
lLycan <lLycan@hotmail.de>
2016-12-06 18:51:48 +0100

    added comments tu uiController.ads

--- src/uiController.ads
+++ src/uiController.ads
@@ -6,7 +6,13 @@ with Param;                      use Param;
 with Search;			 use Search;
 with Cli;                        use Cli;
 
+--The uiController manages the call of the file search and the output throught
+--the given parameters.
 Package UIController is
+   --opens the file search if the parameter for the help isnt set.
    procedure startFileSearch( parameter : Param.params );
+
+   --starts the output on the cli. The variable vec safes the search result while
+   --the variable parameter gives the chosen parameters.
    procedure startUI( vec : FileVector.Vector; parameter : Param.params );
 end UIController;
