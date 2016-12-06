7b2bcacc30ffcfa109ecac420ed629e073e3fd24
adnors <sandro.kaeppner@gmail.com>
2016-12-06 18:44:57 +0100

    Comments added

--- src/GlobalTypes.adb
+++ src/GlobalTypes.adb
@@ -1,10 +1,14 @@
 Package body GlobalTypes is
 
+   --checks whether the first and the second date are equal, the first date can
+   --have '?' as wildcards
+   --returns true when the two dates are equal
    function "="(date1,date2 : DateType) return Boolean
    is
       isEqual : Boolean := True;
    begin
       for i in 0 .. date1'Length - 1 loop
+         --check for a wildcard
          if date1(date1'First + i) /= '?' then
             if date1(date1'First + i) /= date2(date2'First + i) then
                isEqual := False;
@@ -15,11 +19,15 @@ Package body GlobalTypes is
       return isEqual;
    end "=";
 
+   --checks whether the first date is earlier (smaller) than the second date,
+   --the first date can have '?' as wildcards
+   --returns true when the first date is earlier than the second date
    function "<"(date1, date2 : DateType) return Boolean
    is
       isEarlier : Boolean := True;
    begin
       for i in 0 .. date1'Length - 1 loop
+         --check for a wildcard
          if date1(date1'First + i) /= '?' then
             if date1(date1'First + i) > date2(date2'First + i) then
                isEarlier := False;
@@ -27,6 +35,7 @@ Package body GlobalTypes is
          end if;
       end loop;
 
+      --check for equal dates
       if isEarlier and then
         date1 = date2 then
          isEarlier := False;
@@ -35,11 +44,15 @@ Package body GlobalTypes is
       return isEarlier;
    end "<";
 
+   --checks whether the first date is later (bigger) than the second date, the
+   --first date can have '?' as wildcards
+   --returns true when the first date is later than the second date
    function ">"(date1, date2 : DateType) return Boolean
    is
       isLater : Boolean := True;
    begin
       for i in 0 .. date1'Length - 1 loop
+         --check for a wildcard
          if date1(date1'First + i) /= '?' then
             if date1(date1'First + i) < date2(date2'First + i) then
                isLater := False;
@@ -47,6 +60,7 @@ Package body GlobalTypes is
          end if;
       end loop;
 
+      --check for equal dates
       if isLater and then
         date1 = date2 then
          isLater := False;
@@ -55,11 +69,15 @@ Package body GlobalTypes is
       return isLater;
    end ">";
 
+   --checks whether the first time is earlier (smaller) than the second time,
+   --the first time can have '?' as wildcards
+   --returns true when the first time is earlier than the second time
    function "<"(time1, time2 : TimeType) return Boolean
    is
       isEarlier : Boolean := True;
    begin
       for i in 0 .. time1'Length - 1 loop
+         --check for a wildcard
          if time1(time1'First + i) /= '?' then
             if time1(time1'First + i) > time2(time2'First + i) then
                isEarlier := False;
@@ -67,6 +85,7 @@ Package body GlobalTypes is
          end if;
       end loop;
 
+      --check for equal times
       if isEarlier and then
         time1 = time2 then
          isEarlier := False;
@@ -75,11 +94,15 @@ Package body GlobalTypes is
       return isEarlier;
    end "<";
 
+   --checks whether the first time is later (bigger) than the second time, the
+   --first time can have '?' as wildcards
+   --returns true when the first time is later than the second time
    function ">"(time1, time2 : TimeType) return Boolean
    is
       isLater : Boolean := True;
    begin
       for i in 0 .. time1'Length loop
+         --check for a wildcard
          if time1(time1'First + i) /= '?' then
             if time1(time1'First + i) < time2(time2'First + i) then
                isLater := False;
@@ -87,6 +110,7 @@ Package body GlobalTypes is
          end if;
       end loop;
 
+      --check for equal times
       if isLater and then
         time1 = time2 then
          isLater := False;
--- src/search.adb
+++ src/search.adb
@@ -1,6 +1,10 @@
 Package body Search is
-   --See https://rosettacode.org/wiki/Walk_a_directory/Non-recursively#Ada
+
+
    function searchDirectory (parameters : Param.params) return FileVector.Vector
+   --Searches for image files, with the parameters provided
+   --returns a vector with the images that fit the parameters
+   --See https://rosettacode.org/wiki/Walk_a_directory/Non-recursively#Ada
    is
       parametersCopy : Param.params;
       currentSearch : Directories.Search_Type;
@@ -10,7 +14,7 @@ Package body Search is
       addFileToVector : Boolean := True;
    begin
 
-      --get all files in the directory
+      --search the files in the directory
       --files that don't fit the filename parameter, will not be returned
       Directories.Start_Search(Search    => currentSearch,
                                Directory => Ada.Strings.Unbounded.To_String(Ada.Strings.Unbounded.Unbounded_String(parameters.directory)),
@@ -65,46 +69,58 @@ Package body Search is
             end if;
          exception
             when others =>
-               --the exception is just for restarting the loop
+               --catch the exception from ImageFile.create()
                null;
          end;
       end loop;
 
+      --end the search and return the vector
       Directories.End_Search(currentSearch);
       return fileVectorObj;
    end searchDirectory;
 
    function checkParameters(parameters : Param.params;
                             image : ImageFile.ExifDataAccess) return Boolean
+   --check whether the image fits to the parameters
+   --returns true if the image fits the parameters that aren't on their default
+   --value
    is
       fileFitsParameters : Boolean := True;
    begin
-      --check whether the image fits to the parameters
       --if the values of the parameters are on their default values then they
       --don't have to be checked
+
+      --check for parameter date
       if parameters.date /= "          " and then
         parameters.date = image.date then
          fileFitsParameters := False;
       end if;
 
+      --if the file already doesn't fit the parameters the rest of the
+      --parameters are not checked anymore
+
+      --check for parameter image width
       if fileFitsParameters and then
         parameters.imageSizeX /= 1 and then
         parameters.imageSizeX > image.imageWidth then
          fileFitsParameters := False;
       end if;
 
+      --check for parameter image height
       if fileFitsParameters and then
         parameters.imageSizeY /= 1 and then
         parameters.imageSizeY > image.imageHeight then
          fileFitsParameters := False;
       end if;
 
+      --check for parameter size
       if fileFitsParameters and then
         parameters.fileSize /= 1 and then
         parameters.fileSize > image.fileSize then
          fileFitsParameters := False;
       end if;
 
+      --check for parameter date range
       if fileFitsParameters and then
         parameters.dateRangeStart /= "          " and then
         parameters.dateRangeStart > image.date then
@@ -117,6 +133,7 @@ Package body Search is
          fileFitsParameters := False;
       end if;
 
+      --check for parameter time
       if fileFitsParameters and then
         parameters.dateTime /= "        " and then
         parameters.dateTime > image.time then
@@ -126,8 +143,11 @@ Package body Search is
       return fileFitsParameters;
    end checkParameters;
 
+
+   function "="(imageFile1, imageFile2 : ImageFile.ExifDataAccess) return Boolean
    --function for the use with vectors
-   function "="(imageFile1, imageFile2 : ImageFile.ExifDataAccess) return Boolean is
+   --returns true if the two image files have the same file name
+   is
    begin
       if imageFile1 /= null and then imageFile2 /= null and then imageFile1.filename = imageFile2.filename then
          return True;
